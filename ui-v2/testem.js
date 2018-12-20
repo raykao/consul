6c9992289fe876dec28bdf27057d0de91c0f093a
Alvin Huang <alvin@hashicorp.com>
2018-12-20 12:52:08 -0500

    update testem with newer conventions

--- ui-v2/testem.js
+++ ui-v2/testem.js
@@ -4,17 +4,24 @@ module.exports = {
   launch_in_ci: ['Chrome'],
   launch_in_dev: ['Chrome'],
   browser_args: {
-    Chrome: {
-      mode: 'ci',
-      args: [
-        // --no-sandbox is needed when running Chrome inside a container
-        process.env.TRAVIS ? '--no-sandbox' : null,
-
+    Chrome: [
+        '--no-sandbox',
         '--disable-gpu',
         '--headless',
         '--remote-debugging-port=0',
-        '--window-size=1440,900',
-      ].filter(Boolean),
-    },
-  },
+        '--window-size=1440,900'
+    ]
+  }
 };
+
+/*
+ * ember-exam honors the `parallel` parameter in testem.js.
+ * By default this value is 1 which means it only uses one client.
+ * When this is set to -1 it uses the --split value of ember-exam.
+ *
+ * https://github.com/trentmwillis/ember-exam#split-test-parallelization
+ * https://github.com/trentmwillis/ember-exam/issues/108
+ */
+if (process.env.EMBER_EXAM_PARALLEL) {
+  module.exports.parallel = -1
+}
